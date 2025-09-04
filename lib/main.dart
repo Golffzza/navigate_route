9fdf8f5899d39cc2e492fa1675114113694cbfe3
Golffzza <golffyloanna@gmail.com>
2025-09-04 20:18:52 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:navigate_route/screens/detail_screen.dart';
+import 'package:navigate_route/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: HomeScreen.routeName, // ชื่อ route เริ่มต้น
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        DetailScreen.routeName: (context) => const DetailScreen(),
+        // เพิ่ม screens อื่นๆ ที่นี่ภายหลัง
+      },
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:navigate_route/screens/detail_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = '/detail';
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                Navigator.pushNamed(
+                  context,
+                  DetailScreen.routeName,
+                  arguments: {
+                    // ส่ง arguments เป็น Map
+                    'id': 'GOLF007',
+                    'title': 'ข้อมูลจาก Named Route Args',
+                  },
+                );
+              },
+              child: const Text('Go to Detail Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
